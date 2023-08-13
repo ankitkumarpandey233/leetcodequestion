613f338fcb05f25fef93441ba71d0bc39a2c91bd
Ankit Kumar Pandey <98585370+ankitkumarpandey233@users.noreply.github.com>
2023-08-13 15:16:53 +0530

    Time: 2 ms (91.36%), Space: 44.9 MB (52.65%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.java
+++ 0495-teemo-attacking/0495-teemo-attacking.java
@@ -1,20 +1,12 @@
 class Solution 
 {
-  public int findPoisonedDuration(int[] timeSeries, int duration) 
+    public int findPoisonedDuration(int[] timeSeries, int duration) 
     {
-      int total = 0;      
-        for (int i = 0; i < timeSeries.length - 1; i++) 
-        {
-          int timeDiff = timeSeries[i + 1] - timeSeries[i];
-          total += Math.min(duration, timeDiff);
-        }
-        
-        if (timeSeries.length > 0) 
-        {
-          total += duration;
-        }
-        
+      int total=duration;
+      for(int i=1;i<timeSeries.length;i++)
+      {
+        total+=Math.min(duration, timeSeries[i]-timeSeries[i-1])  ;
+      }
       return total;
-    
     }
 }
