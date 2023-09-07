c967869d61c5fec5a8f34fb3e18c3021e4d81974
Ankit Kumar Pandey <98585370+ankitkumarpandey233@users.noreply.github.com>
2023-09-08 00:11:01 +0530

    Time: 0 ms (100%), Space: 41 MB (24.11%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,13 +1,20 @@
-class Solution {
-    public int lengthOfLastWord(String s) {
-       int i = s.length() - 1, length = 0;
-       while (s.charAt(i) == ' ') {
-           i -= 1;
-       }
-       while (i >= 0 && s.charAt(i) != ' ') {
-           length += 1;
-           i -= 1;
-       } 
-       return length;
+class Solution 
+{
+    public int lengthOfLastWord(String s) 
+    {
+        int l =0;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            if(s.charAt(i)!=' ')
+            {
+                l++;
+            }
+            else
+            {
+                if (l>0)
+                return l;
+            }
+        }
+        return l;
     }
 }
