a2fe33a64f561bb00b212d02e6816843b8a1c8bb
Ankit Kumar Pandey <98585370+ankitkumarpandey233@users.noreply.github.com>
2023-09-09 15:22:43 +0530

    Time: 6 ms (80.83%), Space: 46.6 MB (88.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -6,17 +6,10 @@ class Solution {
             char[] charArray = str.toCharArray();
             Arrays.sort(charArray);
             String sorted = new String(charArray);
-
-            // Check if the sorted string is a key in the HashMap
-            // If it's not, create a new entry with the sorted string as the key and a new list as the value
             anagramGroups.putIfAbsent(sorted, new ArrayList<>());
-
-            // Add the current string to the list of values for the sorted string key
             anagramGroups.get(sorted).add(str);
         }
-
-        // Convert the values of the HashMap into a list of lists and return it
         return new ArrayList<>(anagramGroups.values());
     }
         
-    }
+}
