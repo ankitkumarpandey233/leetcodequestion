1e57adebc3af71eda3e21d99f25d5574ba5c6d9c
Ankit Kumar Pandey <98585370+ankitkumarpandey233@users.noreply.github.com>
2023-09-16 14:33:04 +0530

    Time: 11 ms (52.5%), Space: 41.6 MB (30.64%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,24 +1,31 @@
-class Solution 
-{
-    public boolean isIsomorphic(String s, String t) 
-    {
-        int map1[] = new int[200];
-        int map2[] = new int[200];
-
-      if (s.length() != t.length())
-      {
-        return false;
-      }
-      
-
-     for (int i = 0; i < s.length(); i++) 
-        {
-          if (map1[s.charAt(i)] != map2[t.charAt(i)])
-           return false;
-
-           map1[s.charAt(i)] = i + 1;
-           map2[t.charAt(i)] = i + 1;
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+         if (s.length() != t.length()) {
+            return false;
         }
+
+        HashMap<Character, Character> map1 = new HashMap<>();
+        HashMap<Character, Boolean> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            if (map1.containsKey(sChar)) {
+                if (map1.get(sChar) != tChar) {
+                    return false;
+                }
+            } else {
+                if (map2.containsKey(tChar) && map2.get(tChar)) {
+                    return false;
+                }
+
+                map1.put(sChar, tChar);
+                map2.put(tChar, true);
+            }
+        }
+
         return true;
+    
     }
 }
